3fa93d64e9d75968e7255ecb795edd63307adaf5
scarla-rgb <scarlarodriguez0620@gmail.com>
2021-02-27 17:04:54 +0100

    primer commit remoto

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const saludar = (nombre) => {
+
+    console.log ('creando etiqueta h1, en el HTML');
+
+    // Comentario de una sola linea.
+    // El codigo comentado no lo lee el interprete de JS.
+
+    /*
+        Comentario de varias lineas.
+        Uso Basico del DOCUMENTO OBJECT MODEL (DOM).
+        Permite a JS modificar el HTML.
+    */
+
+   const h1 = document.createElement ('h1');
+   h1.innerText = 'Hola, ${nombre}';
+
+   document.body.append (h1);
+
+}
+
+const user = "Juancho";
+saludar (user);
